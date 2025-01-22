e1f70eb82d4ef81b08e1f5fd2ecefae25de112cc
Stiveys <stevomwaura@gmail.com>
2025-01-22 11:37:40 +0300

    new

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -2,8 +2,6 @@ import { currentAgeForBirthYear } from "../utils";
 
 describe("currentAgeForBirthYear", () => {
   it("returns the age of a person based on the year of birth", () => {
-    const birthYear = 1984;
-    const ageOfPerson = currentAgeForBirthYear(birthYear);
-    expect(ageOfPerson).toBe(38);
+    // test implementation goes here
   });
 });
